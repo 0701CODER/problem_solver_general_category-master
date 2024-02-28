1c6c0d0a19c8c4ff1daaa809575b3f2d638b1298
vasanth <maxdelz30@gmail.com>
2024-02-28 12:54:47 +0530

    first commit

---
+++ check/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from upload.models import upload
+from transfer.models import transactions
+# Create your views here.
+def check(request):
+    if request.method=="POST":
+        id=request.POST.get("code")
+        lis=upload.objects.all()
+        a=transactions.objects.filter(code=id)
+
+        for n in lis:
+            if n.code==id:
+                context={"prod":n,"data":a}
+                found=True
+                break
+            else:
+                found=False
+        if found:
+            return render(request,"result.html",context)
+        else:
+            return render(request,"failed.html",{})
+
+    return render(request,"check.html",{})
